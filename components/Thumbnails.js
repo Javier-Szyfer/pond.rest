f187049e4b229fa2b5b5e88a7d271fe8b7c90b07
Javier Szyfer <jszyfer@gmail.com>
2021-06-23 18:55:40 -0300

    custom colors

--- components/Thumbnails.js
+++ components/Thumbnails.js
@@ -1,27 +1,44 @@
 import { useState, useContext } from "react";
 
-import { SimpleGrid, Box, Text } from "@chakra-ui/react";
+import { SimpleGrid, Box, Text, useColorMode } from "@chakra-ui/react";
 import { formatDistance, parseISO } from "date-fns";
 
 import { MusicPlayerContext } from "../context/AudioContext";
 import useMusicPlayer from "../hooks/useMusicPlayer";
 
 export default function Thumbnails({ onToggle, isOpen }) {
+  const { colorMode, toggleColorMode } = useColorMode();
   const [state] = useContext(MusicPlayerContext);
   const { playTrack, selectedTrack, setIsPlaying } = useMusicPlayer();
 
-  const colors = (i) => {
+  //Light colors
+
+  const colorsLight = (i) => {
     if (i >= 0 && i <= 3) {
-      return arrayOfColors[0];
+      return arrayOfColorsLight[0];
     } else if (i >= 4 && i <= 7) {
-      return arrayOfColors[1];
+      return arrayOfColorsLight[1];
     } else if (i >= 8 && i <= 11) {
-      return arrayOfColors[2];
+      return arrayOfColorsLight[2];
     } else if (i >= 12 && i <= 15) {
-      return arrayOfColors[3];
+      return arrayOfColorsLight[3];
     }
   };
-  const arrayOfColors = ["#88D18A", "#F0B7B3", "#ECCE8E", "#9AC2C5"];
+  const arrayOfColorsLight = ["#88D18A", "#F0B7B3", "#ECCE8E", "#9AC2C5"];
+
+  //Dark colors
+  const colorsDark = (i) => {
+    if (i >= 0 && i <= 3) {
+      return arrayOfColorsDark[0];
+    } else if (i >= 4 && i <= 7) {
+      return arrayOfColorsDark[1];
+    } else if (i >= 8 && i <= 11) {
+      return arrayOfColorsDark[2];
+    } else if (i >= 12 && i <= 15) {
+      return arrayOfColorsDark[3];
+    }
+  };
+  const arrayOfColorsDark = ["#5F5B6B", "#3D3B3C", "#323031", "#323031"];
 
   const handlePlay = (tr) => {
     playTrack(tr);
@@ -45,7 +62,7 @@ export default function Thumbnails({ onToggle, isOpen }) {
           minH="100px"
           w="100%"
           h="100%"
-          bg={colors(i)}
+          bg={colorMode === "light" ? colorsLight(i) : colorsDark(i)}
           draggable
           boxShadow="rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px"
           onClick={() => handlePlay(tr)}
